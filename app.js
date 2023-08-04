0923a0f9e7135fffdd3e578b5a70aeb14770bc0d
Heber <116190313+hpagon@users.noreply.github.com>
2023-08-04 15:58:17 -0700

    Slider can now edit the size of the board

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const container = document.querySelector("#container");
+const slider = document.querySelector("#size");
+const sliderLabel = document.querySelector("#sliderLabel");
+let squares;
+let rows;
 
-let size = 32;
-
-for (let i = 0; i < size; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < size; j++) {
-    let newSquare = document.createElement("div");
-    newSquare.classList.add("square");
-    row.appendChild(newSquare);
+let size = 16;
+function drawBoard() {
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < size; j++) {
+      let newSquare = document.createElement("div");
+      newSquare.classList.add("square");
+      row.appendChild(newSquare);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
+  squares = document.querySelectorAll(".square");
+  rows = document.querySelectorAll(".row");
 }
 
-let squares = document.querySelectorAll(".square");
+drawBoard();
+
+function eraseBoard() {
+  rows.forEach((row) => {
+    container.removeChild(row);
+  });
+}
 
 container.addEventListener("mousedown", (event) => {
   console.log("mouse down");
-  event.preventDefault()
+  event.preventDefault();
   held();
 });
 
@@ -41,3 +54,15 @@ function released() {
     square.removeEventListener("mouseenter", blackPen);
   });
 }
+
+slider.addEventListener("change", () => {
+//   sliderLabel.textContent = `Grid Size: ${slider.value} x ${slider.value}`;
+//   size = slider.value;
+  eraseBoard();
+  drawBoard();
+});
+
+slider.addEventListener("input", () => {
+  sliderLabel.textContent = `Grid Size: ${slider.value} x ${slider.value}`;
+  size = slider.value;
+});
